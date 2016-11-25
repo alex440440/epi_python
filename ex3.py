b63bf91e449d2300e68ca44b79d9149df48451b0
Lena <lenabru@gmail.com>
2016-11-25 21:47:37 +0200

    6.9

---
+++ ex3.py
@@ -0,0 +1,11 @@
+def foo():
+    x = 1
+    if x == 1:
+        # indented four spaces
+        print "x is 1."
+
+def main():
+    foo()
+
+if __name__ == "__main__":
+    main()
---
+++ test_ex6_9.py
@@ -0,0 +1,28 @@
+import unittest
+from Ex6_9 import MyClass
+
+class TestStringMethods(unittest.TestCase):
+    my_class = MyClass()
+
+    def test_deltas(self):
+        ar = [1, 2, 3]
+        deltas = self.my_class.get_deltas(ar)
+        self.assertEquals(deltas, [1, 1])
+
+
+    def test1(self):
+        ar = [4,5,6,10]
+        find_max = self.my_class.find_max(ar, 1)
+        print("find_max: ", find_max)
+        self.assertEqual(find_max, (6, 0, 3))
+
+
+    def test2(self):
+        ar = [2,3,4,5,4,3,2,0,1,15,3,15,0]
+        find_max = self.my_class.find_max(ar, 1)
+        print("find_max: ", find_max)
+        self.assertEqual(find_max, (15, 7, 9))
+
+
+if __name__ == '__main__':
+    unittest.main()
