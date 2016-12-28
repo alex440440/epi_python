52fc30a15ff1c8c470edcdf49c3fd42285ee4cbf
Alex <alex440@gmail.com>
2016-12-29 01:14:37 +0200

    6.6.0 - java book

--- 6.6.0/e_6_6_0.py
+++ 6.6.0/e_6_6_0.py
@@ -4,11 +4,25 @@ import sys
 class Main:
     @staticmethod
     def find_max(ar):
-        max_sum = 0;
+        max_sum = 0
         min_cost = sys.maxint
         for cost in ar:
             max_sum = max(max_sum, cost - min_cost)
             min_cost = min(min_cost, cost)
+        return max_sum
+
+    @staticmethod
+    def find_longest_subarray(ar):
+        max_sum = 0
+        current_length = 0
+        prev = None
+        for i in ar:
+            if i == prev:
+                current_length += 1
+            else:
+                current_length = 1
+                prev = i
+            max_sum = max(current_length, max_sum)
         return max_sum
 
 
--- 6.6.0/t_6_6_0.py
+++ 6.6.0/t_6_6_0.py
@@ -1,7 +1,6 @@
 import unittest
 from e_6_6_0 import Main
 
-
 class Test_660(unittest.TestCase):
 
     def test1(self):
@@ -10,12 +9,30 @@ class Test_660(unittest.TestCase):
         print("find_max: ", find_max)
         self.assertEqual(find_max, 6)
 
-
     def test2(self):
-        ar = [310,315,275,295,260,270,290,230,255,250]
+        ar = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
         find_max = Main.find_max(ar)
         print("find_max: ", find_max)
         self.assertEqual(find_max, 30)
+
+    def test3(self):
+        ar = [1,2,3,4,5]
+        longest = Main.find_longest_subarray(ar)
+        print("longest: ", longest)
+        self.assertEqual(longest, 1)
+
+    def test4(self):
+        ar = [1,1,1,2,3,4,4,5,5]
+        longest = Main.find_longest_subarray(ar)
+        print("longest: ", longest)
+        self.assertEqual(longest, 3)
+
+
+    def test5(self):
+        ar = [1,1,1,2,3,4,4,5,5,5,5]
+        longest = Main.find_longest_subarray(ar)
+        print("longest: ", longest)
+        self.assertEqual(longest, 4)
 
 if __name__ == '__main__':
     unittest.main()
