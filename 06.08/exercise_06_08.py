4e7ff9eb691fc6ee7a48aa34b775f350bde704e6
Alex <alex440@gmail.com>
2017-01-03 00:19:35 +0200

    ex 6.8

--- 06.08/exercise_06_08.py
+++ 06.08/exercise_06_08.py
@@ -5,6 +5,10 @@ class Main:
 
     @staticmethod
     def get_primes(number):
+        return Main.get_primes_sieving(number)
+
+    @staticmethod
+    def get_primes_division(number):
         primes=[]
         for num in xrange(2,number+1):
             if not Main.is_divisable(num, primes):
@@ -20,6 +24,17 @@ class Main:
                 return True
         return False
 
+    @staticmethod
+    def get_primes_sieving(number):
+        primes=[]
+        is_prime = [True] * number
+        is_prime[1] = False
+        for num in xrange(2, number):
+            if is_prime[num]:
+                primes.append(num)
+                for i in xrange(num,number,num):
+                    is_prime[i]=False
+        return primes
 
 if __name__ == "__main__":
     Main().main()
