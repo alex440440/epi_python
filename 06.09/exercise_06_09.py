955b96e14f6ee6e82aec3ac0c21babce634445b7
Alex <alex440@gmail.com>
2017-01-06 06:06:55 +0200

    ex 6.9 permutations

--- 06.09/exercise_06_09.py
+++ 06.09/exercise_06_09.py
@@ -1,7 +1,7 @@
 import sys
 
-class Main:
 
+class Main:
     # @staticmethod
     # def apply_permutation_simple(array,permutation):
     #     out=[''] * len(array)
@@ -10,24 +10,86 @@ class Main:
     #         out[permutation[i]]=array[i]
     #     return out
 
+    # also O(n) - because is changing the permutation array and there is no way to restore it.
+    # @staticmethod
+    # def apply_permutation(array, permutation):
+    #     for i in xrange(len(array)):
+    #         Main.swap(array, i, permutation[i])
+    #         Main.swap(permutation, i, permutation[i])
+    #     return array
+
     @staticmethod
     def apply_permutation(array, permutation):
-        for i in xrange(len(array)):
-            Main.swap(array, i, permutation[i])
-            Main.swap(permutation, i, permutation[i])
+        print("permutation: "+str(permutation))
+
+        array_length = len(array)
+        for cycle_start in xrange(array_length):
+            # initial index of the cycle
+            move_from = cycle_start
+            # get the index we need to move the first member of the cycle
+            move_to = permutation[move_from]
+            permutation[move_from] -= array_length
+            while (True):
+                print("cycle_start: "+str(cycle_start)+", move_from: "+str(move_from)+", move_to: "+str(move_to))
+                print("permutation: "+str(permutation))
+
+                # we go in a cyclical fashion to the next member we can move,
+                # till we arrive to the member that should be moved to the place of one we already moved
+                # if it's the case, we arrived to the cycle end and should move to the next cycle.
+                is_new_permutation = move_to >= 0
+                if is_new_permutation:
+                    print("We are looking at a permutation which was not yet performed")
+
+                    # swap here
+                    # move_from -> move_to
+                    # move_to -> move_from
+                    Main.swap(array, move_from, move_to)
+
+
+                    # we just swapped this member with the one we placed from move_from,
+                    # so the next member we are going to move is the misplaced one at move_from
+                    # and its destination is stored in the permutation array where he was before we swapped it - move_to
+                    old_permutation_index=move_to
+                    move_to = permutation[move_to]
+
+                    # mark the permutation as one we already performed
+                    permutation[old_permutation_index] -= array_length
+                else:
+                    print("We are looking at a permutation which was already performed, let's move on")
+                    break
+
+        # restore permutation values
+        for p in permutation:
+            p += array_length
+
         return array
 
-    @staticmethod
-    def inverse_permutation(permutation):
-        apply_permutation = Main.apply_permutation(permutation, permutation)
-        return apply_permutation[::-1]
+    # @staticmethod
+    # def inverse_permutation(permutation):
+    #     value = 0
+    #     while value >= 0:
+    #         print("checking for i: " + str(value) + ":" + str(permutation[value]))
+    #         index = permutation[value]
+    #         tmp_value = permutation[index]
+    #         permutation[index] = -value
+    #         value = tmp_value
+    #
+    #     for i in range(len(permutation)):
+    #         permutation[i] = -permutation[i]
+    #
+    #     return permutation
 
+    @staticmethod
+    def get_zero(permutation):
+        return -len(permutation)
 
     @staticmethod
     def swap(array, i, j):
+        print ("swapping "+array[i]+":"+str(i)+" with "+array[j]+":"+str(j))
         tmp = array[i]
-        array[i]=array[j]
-        array[j]=tmp
+        array[i] = array[j]
+        array[j] = tmp
+
 
 if __name__ == "__main__":
     Main().main()
--- 06.09/test_06_09.py
+++ 06.09/test_06_09.py
@@ -4,25 +4,30 @@ from exercise_06_09 import Main
 class Test_06_09(unittest.TestCase):
 
     def test1(self):
-        out = Main.apply_permutation(['a','b','c','d'],[2,0,1,3])
-        print ("permutations are "+str(out))
-        self.assertEqual(out, ['b','c','a','d'])
+        out = Main.apply_permutation(['a', 'b', 'c', 'd'], [2, 0, 1, 3])
+        print ("permutations are " + str(out))
+        self.assertEqual(out, ['b', 'c', 'a', 'd'])
 
     def test2(self):
-        out = Main.apply_permutation(['a','b','c','d'],[3,2,1,0])
-        print ("permutations are "+str(out))
-        self.assertEqual(out, ['d','c','b','a'])
+        out = Main.apply_permutation(['a', 'b', 'c', 'd'], [3, 2, 1, 0])
+        print ("permutations are " + str(out))
+        self.assertEqual(out, ['d', 'c', 'b', 'a'])
 
     def test3(self):
-        array = ['a', 'b', 'c', 'd']
-        array_copy= array[:]
-        permutation = [3, 2, 1, 0]
-        permutation_copy = permutation[:]
-        permuted = Main.apply_permutation(array, permutation)
-        back = Main.apply_permutation(permuted,Main.inverse_permutation(permutation_copy))
-        print ("permutations are "+str(back))
-        self.assertEqual(back,array_copy)
+        out = Main.apply_permutation(['a', 'b', 'c', 'd', 'e'], [2, 1, 0, 4, 3])
+        print ("permutations are " + str(out))
+        self.assertEqual(out, ['c', 'b', 'a', 'e', 'd'])
 
+    #
+    # def test4(self):
+    #     array = ['a', 'b', 'c', 'd']
+    #     array_copy= array[:]
+    #     permutation = [2,0,1,3]
+    #     permutation_copy = permutation[:]
+    #     permuted = Main.apply_permutation(array, permutation)
+    #     back = Main.apply_permutation(permuted,Main.inverse_permutation(permutation_copy))
+    #     print ("permutations are "+str(back))
+    #     self.assertEqual(back,array_copy)
 
 
 if __name__ == '__main__':
